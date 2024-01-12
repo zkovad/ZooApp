2b21dcede90b9e2131c4d1d1be29d1ac83b51864
esilva <esilva@ethz.ch>
2024-01-12 08:18:48 +0100

    Main setup

---
+++ src/components/Parking.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Parking = () => {
+  return (
+    <div>
+      <h2>parking Page</h2>
+      {/* Your home page content */}
+    </div>
+  );
+};
+
+export default Parking;
---
+++ src/components/Tickets.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Tickets = () => {
+  return (
+    <div>
+      <h2>Tickets Page</h2>
+      {/* Your home page content */}
+    </div>
+  );
+};
+
+export default Tickets;
