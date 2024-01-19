2d79b990220cb42e07e543c4b191feba13d16307
esilva <esilva@ethz.ch>
2024-01-19 08:06:28 +0100

    Imports fix

--- src/components/Parking.js
+++ src/components/Parking.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Parkinglot from './Parkinglot.jpg'
-import './Parking.css';
+import Parkinglot from '../assets/images/Parkinglot.jpg'
+import '../assets/css/Parking.css';
 import { Link } from 'react-router-dom';
 
 
